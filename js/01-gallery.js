e29a32c3cedc57057e97748ae64b6f8a9f242665
KmiloLopez <108908318+KmiloLopez@users.noreply.github.com>
2023-02-22 18:16:53 -0500

    basic lightbox and simple lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,72 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+console.log(galleryItems[0])
+
+const divRef = document.querySelector('.gallery')
+
+function createGalleryM(items){
+    return items
+        .map(
+            (item)=> `<div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                    />
+                    </a>
+                    </div>`
+        )
+        .join("")
+}
+const addgalleryM = createGalleryM(galleryItems)
+divRef.innerHTML=addgalleryM
+divRef.addEventListener('click',onImageClick)
+function onImageClick(event){
+    blockStardartAction(event)
+    if(event.target.nodeName !== "IMG"){
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `)
+    instance.show();
+
+    divRef.addEventListener("keydown", (event) => {
+        if(event.code === "Escape"){
+            instance.close();
+        }
+    })
+}
+function blockStardartAction(event){
+    event.preventDefault()
+}
+/* const gallerySection = document.getElementsByClassName('gallery') */
+/* const gallery = document.querySelector('.gallery')
+
+galleryItems.map((element)=>{
+    let link = document.createElement('a')
+    console.log(link)
+    link.href=element.original
+
+    let image = document.createElement('img')
+    image.src = element.preview
+    image.alt = element.description
+
+    link.appendChild(image)
+    gallery.appendChild(link)
+
+    
+}) */
+document.querySelector('.gallery.firstchild').onclick = () => {
+
+	basicLightbox.create(`
+		<img width="1400" height="900" src="https://placehold.it/1400x900">
+	`).show()
+
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,46 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divRef = document.querySelector('.gallery')
+
+function createGalleryM(items){
+    return items
+        .map(
+            (item)=> `
+            <a class="gallery__item" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    
+                    alt="${item.description}"
+                    />
+                    </a>
+                    </div>`
+        )
+        .join("")
+}
+const addgalleryM = createGalleryM(galleryItems)
+divRef.innerHTML=addgalleryM
+divRef.addEventListener('click',onImageClick)
+function onImageClick(event){
+    blockStardartAction(event)
+    if(event.target.nodeName !== "IMG"){
+        return;
+    }
+    var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+    
+    /* const instance = simpleLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `)
+    instance.show(); */
+
+    divRef.addEventListener("keydown", (event) => {
+        if(event.code === "Escape"){
+            instance.close();
+        }
+    })
+}
+function blockStardartAction(event){
+    event.preventDefault()
+}
